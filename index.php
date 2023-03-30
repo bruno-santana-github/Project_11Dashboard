db2843c1be6e0b0d5a99da350ba5de350eb5d6bb
Bruno Santana <brunomoraes.bm.bm@gmail.com>
2023-03-29 22:13:14 -0300

    Project11 - Dashboard

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+        <link href="css/template.css" type="text/css" rel="stylesheet"/>
+        <title>Project 10 | Dashboard</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="keywords" content="bruno s. moraes, bruno, moraes, bruno santana, descomplica university, descomplica, university, student, front-end, back-end, fullstack,  web, website, computer, software, developer, engineer, designer, co-op" />
+        <meta name="author" content="Bruno Moraes" />
+        <meta name="description" content="Fullstack Developer Jr,  Enthusiast, and everything in between." />
+        <meta property="og:description" content="Fullstack Developer, Enthusiast, and everything in between." />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0"/>
+        <meta name="viewport" content="width=device-width"/>
+        <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
+        <meta name="author" content="Bruno Santana"/>
+        <!----------Tags SEO Facebook--------->
+        <meta property="og:url"                content="" />
+        <meta property="og:type"               content="" />
+        <meta property="og:title"              content="" />
+        <meta prHow much does culture influence creative thinking?" />
+        <meta property="og:image"              content="" />
+        <!----------Tags SEO Twitter--------->
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="" />
+        <meta name="twitter:creator" content="" />
+        <meta property="og:url" content="" />
+        <meta property="og:title" content="A Twitter for My Sister" />
+        <meta property="og:description" content="" />
+        <meta property="og:image" content="" />
+        <!----------Links de Style--------->
+        <link href="images/logo.png" type="image-x/png" rel="shortcut icon"/>
+    </head>
+    <body>
+        <div class="sidebar">
+            <div class="topo">
+                <h3>Bdeveloper</h3>
+            </div><!---topo-->
+            <div class="menu">
+                <ul>
+                    <li><i class="fa fa-home" aria-hidden="true"></i><a href="#">Home</a></li>
+                    <li><i class="fa fa-id-card" aria-hidden="true"></i><a href="#">About</a></li>
+                    <li><i class="fa fa-phone" aria-hidden="true"></i><a href="#">Contacts</a></li>
+                    <li><i class="fa fa-info" aria-hidden="true"></i><a href="#">Informations</a></li>
+                    <li><i class="fa fa-camera" aria-hidden="true"></i><a href="#">Galery</a></li>
+                </ul>
+            </div><!--menu-->
+        </div><!---sidebar-->
+
+        <div class="main-content">
+            <header>
+                <div class="nome-usuario">
+                <h2><i class="fas fa-align-justify"></i></h2>
+                </div><!---nome-usario-->
+
+                <div class="pesquisa-campo">
+                    <div class="icone-pesquisa"><i class="fas fa-search"></i></div><form><input type="text" name="nome" placeholder="find here..."></form>               
+                </div><!---pesquisa-campo-->
+
+                <div class="icone-alerta">
+                    <p>2</p>
+                    <i class="fas fa-bell"></i>
+                </div><!---icone-alerta-->
+                <div class="clear"></div>
+            </header>
+            <div class="clear"></div>
+
+            <div class="content-1">
+                <div class="panel">
+                    <h2>Welcome, <b>Bruno Santana</b></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed purus libero. Nam iaculis felis sed dignissim tincidunt. Sed eu odio enim. Morbi leo orci, ultricies eget pretium imperdiet, bibendum sit amet dui. Aliquam sit amet consequat ligula. Nam accumsan quis nisl vel maximus. Quisque malesuada mauris sit amet elit feugiat placerat. Vestibulum consequat vel diam a aliquam. Pellentesque iaculis congue dui vitae hendrerit. Mauris consequat facilisis nisi non vulputate. Integer et suscipit nisl. Sed nec ligula erat.</p>
+                </div><!---panel--->   
+                
+                <div class="panel" style="width: 49%;margin-right: 1%;">
+                    <h2>Welcome, <b>Bruno Santana</b></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed purus libero. Nam iaculis felis sed dignissim tincidunt. Sed eu odio enim. Morbi leo orci, ultricies eget pretium imperdiet, bibendum sit amet dui. Aliquam sit amet consequat ligula. Nam accumsan quis nisl vel maximus. Quisque malesuada mauris sit amet elit feugiat placerat. Vestibulum consequat vel diam a aliquam. Pellentesque iaculis congue dui vitae hendrerit. Mauris consequat facilisis nisi non vulputate. Integer et suscipit nisl. Sed nec ligula erat.</p>
+                </div><!---panel--->   
+
+                <div class="panel" style="width: 49%;margin-left: 1%;">
+                    <h2>My firts, <b>Dataset</b></h2>
+                    <div style="width:100%;">
+                        <canvas id="myChart"></canvas>
+                    </div>
+                </div><!---panel---> 
+                <div class="clear"></div>
+                <div class="panel">
+                    <h2>Welcome, <b>Bruno Santana</b></h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed purus libero. Nam iaculis felis sed dignissim tincidunt. Sed eu odio enim. Morbi leo orci, ultricies eget pretium imperdiet, bibendum sit amet dui. Aliquam sit amet consequat ligula. Nam accumsan quis nisl vel maximus. Quisque malesuada mauris sit amet elit feugiat placerat. Vestibulum consequat vel diam a aliquam. Pellentesque iaculis congue dui vitae hendrerit. Mauris consequat facilisis nisi non vulputate. Integer et suscipit nisl. Sed nec ligula erat.</p>
+                </div><!---panel--->   
+            </div><!---content-1---->
+
+            <div class="ultimas-atividades">
+                <div class="atividades-topo">
+                    <h2>Latest Activities</h2>
+                </div><!---atividade-topo-->
+
+                <div class="atividade-registro">
+                    <div class="circle-atividade"></div>
+                    <h2>2 minutes ago</h2>
+                    <p>A new <b>page</b> has been inserted</p>
+                </div><!---atividade-registro-->
+
+                <div class="atividade-registro">
+                    <div class="circle-atividade"></div>
+                    <h2>2 minutes ago</h2>
+                    <p>A new <b>page</b> has been inserted</p>
+                </div><!---atividade-registro-->
+
+                <div class="atividade-registro">
+                    <div class="circle-atividade"></div>
+                    <h2>2 minutes ago</h2>
+                    <p>A new <b>page</b> has been inserted</p>
+                </div><!---atividade-registro-->
+
+                <div class="atividade-registro">
+                    <div class="circle-atividade"></div>
+                    <h2>2 minutes ago</h2>
+                    <p>A new <b>page</b> has been inserted</p>
+                </div><!---atividade-registro-->
+
+                <div class="atividade-registro">
+                    <div class="circle-atividade"></div>
+                    <h2>2 minutes ago</h2>
+                    <p>A new <b>page</b> has been inserted</p>
+                </div><!---atividade-registro-->
+
+            </div><!---ultimas-atividades-->
+            <div class="clear"></div>
+        </div><!---main-content--->
+
+
+        <script src="js/jquery-3.6.3.min.js"></script>
+            <script>
+                $(function(){
+                    var windowWidth = $(window)[0].innerWidth;
+                    if(windowWidth <= 768){
+                    $('.nome-usuario h2').click(function(){
+                        var el = $('.sidebar');
+                        if(el.is(':visible'))
+                        {
+                            el.hide();
+                            $('.main-content').css('left','0');
+                        }else{
+                            el.show();
+                            $('.main-content').css('left','300px');
+                        }
+                    })
+                }
+
+                $(window).resize(function(){
+                    windowWidth = $(window)[0].innerWidth;
+                })
+
+
+                })
+            </script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script>
+            const ctx = document.getElementById('myChart');
+
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: 'My Profits',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 205, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(201, 203, 207, 0.2)'
+                    ],
+                    borderColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(255, 159, 64)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75, 192, 192)',
+                    'rgb(54, 162, 235)',
+                    'rgb(153, 102, 255)',
+                    'rgb(201, 203, 207)'
+                    ],
+                    borderWidth: 1
+                }]
+                },
+                options: {
+                scales: {
+                    y: {
+                    beginAtZero: true
+                    }
+                }
+                }
+            });
+        </script>
+    </body>
+    
+</html>
